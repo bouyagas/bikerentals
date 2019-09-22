b8482f6d4a59b04a2132fb7552ed7db22589ce9f
mohamed gassama <bouyagassama@gmail.com>
2019-09-22 15:39:15 -0400

    Updates

---
+++ precache-manifest.d2547ac340675d60c4d045fcf055eca1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9c9696a5bedb4f543998084338f258e",
+    "url": "/bikerentals/index.html"
+  },
+  {
+    "revision": "f1cdbe7ba41e3d47eeb7",
+    "url": "/bikerentals/static/css/2.05960da3.chunk.css"
+  },
+  {
+    "revision": "bfd55fc5f3b526e72e3c",
+    "url": "/bikerentals/static/css/main.cf16f943.chunk.css"
+  },
+  {
+    "revision": "f1cdbe7ba41e3d47eeb7",
+    "url": "/bikerentals/static/js/2.877d7dbc.chunk.js"
+  },
+  {
+    "revision": "bfd55fc5f3b526e72e3c",
+    "url": "/bikerentals/static/js/main.35a95ae7.chunk.js"
+  },
+  {
+    "revision": "042995b161cb1a337614",
+    "url": "/bikerentals/static/js/runtime-main.2275756c.js"
+  }
+]);
